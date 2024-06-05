b38e41fdf1882ad04b45db66edee19fe63eaeb8f
enocquet <emmanuelnocquet8@gmail.com>
2024-06-05 11:33:57 +0200

    description for resources

--- variables.tf
+++ variables.tf
@@ -1,4 +1,22 @@
 variable "resources" {
+  type = list(object({
+    name         = string
+    storage      = string
+    labels       = map(string)
+    access_modes = list(string)
+    path         = string
+    node_affinity = object({
+      key      = string
+      operator = string
+      values   = list(string)
+    })
+  }))
+  description = <<EOT
+  Values for the persistent volume and persistent volume claims when in 
+  a bare metal context and provisioner is set to local-path.
+  If a provisioner is available, set the provisioner variable to the 
+  value of the StorageClass for this provisioner.
+  EOT
 }
 
 variable "namespace" {
